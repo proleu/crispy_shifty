9f3ad8a535715826891ce272040d7a8ae50b8411
Adam Broerman <broeada@gmail.com>
2021-11-02 00:55:28 -0700

    02_make_states run

--- projects/crispy_shifty_dimers/02_design_filter.sh
+++ projects/crispy_shifty_dimers/02_design_filter.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 #SBATCH -p long
 #SBATCH --mem=32g
-#SBATCH -c 2
+#SBATCH -c 4
 #SBATCH --output=/home/broerman/projects/crispy_shifty/projects/crispy_shifty_dimers/02_design_filter/head_node.out
 
 source activate /projects/crispy_shifty/envs/crispy
