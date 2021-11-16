5dcf1bd3b2a5d841c4d6c0a37599b1c66a177d76
Adam Broerman <broeada@gmail.com>
2021-11-15 20:07:26 -0800

    Added aspect ratio control

--- crispy_shifty/plotting/utils.py
+++ crispy_shifty/plotting/utils.py
@@ -1,7 +1,7 @@
 # Python standard library
 from typing import *
 
-def histplot_df(df, cols: List[str], bins: Union[int, str] = 'auto', discrete: bool = False, hue: str = None, hue_order: List[str] = None):
+def histplot_df(df, cols: List[str], aspect_ratio: str = 'wide', bins: Union[int, str] = 'auto', discrete: bool = False, hue: str = None, hue_order: List[str] = None):
     import matplotlib.pyplot as plt
     import numpy as np
     import seaborn as sns
@@ -13,14 +13,29 @@ def histplot_df(df, cols: List[str], bins: Union[int, str] = 'auto', discrete: b
         palette="colorblind",  # a color palette that is colorblind friendly!
     )
 
+    # Gets square(ish) dimensions for the number of subplots to make
+    # Still knows to, for example, generate a 1x2 grid for 2 plots, rather than a 2x2 grid
     num_plots = len(cols)
     subplot_cols = int(np.ceil(np.sqrt(num_plots)))
     subplot_rows = int(np.ceil(num_plots / subplot_cols))
 
-    # Adjust the aspect ratio to minimize the number of unused plots
-    sum_rows_cols = subplot_cols + subplot_rows
-    subplot_rows = int(np.ceil((sum_rows_cols - np.sqrt(np.square(sum_rows_cols)-4*num_plots)) / 2))
-    subplot_cols = sum_rows_cols - subplot_rows
+    if aspect_ratio != 'square':
+        # Adjusts the aspect ratio to decrease the number of unused subplots. For example, turns the 3x3 grid generated
+        # for 8 subplots into a 2x4 grid.
+        # Doesn't deviate too far from square dimensions. For example, turns the 4x4 grid generated for 13 subplots into
+        # a 3x5 grid with one fewer empty subplot, but doesn't go all the way to a 1x13 or a 2x6 grid. I actually like this
+        # behavior; a 1x13 grid would be annoying to look at.
+        sum_dims = subplot_cols + subplot_rows
+        short_dim = int(np.ceil((sum_dims - np.sqrt(np.square(sum_dims)-4*num_plots)) / 2))
+        long_dim = sum_dims - short_dim
+        if aspect_ratio == 'wide':
+            subplot_rows = short_dim
+            subplot_cols = long_dim
+        elif aspect_ratio == 'tall':
+            subplot_rows = long_dim
+            subplot_cols = short_dim
+        else:
+            raise ValueError('Must provide a valid aspect ratio')
 
     fig, axs = plt.subplots(subplot_rows, subplot_cols, figsize=(subplot_cols*4, subplot_rows*4))
 
