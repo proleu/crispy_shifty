7f0a895830f6ec5c27f0c2c8ff1dac9642188356
Adam Broerman <broeada@gmail.com>
2021-11-19 22:47:20 -0800

    Sequence design after looping

--- crispy_shifty/protocols/looping.py
+++ crispy_shifty/protocols/looping.py
@@ -241,7 +241,7 @@ def loop_dimer(
     sys.path.insert(0, "/mnt/home/broerman/projects/crispy_shifty")
     from crispy_shifty.protocols.cleaning import path_to_pose_or_ppose
     from crispy_shifty.utils.io import print_timestamp
-    from crispy_shifty.protocols.design import score_wnm, score_ss_sc
+    from crispy_shifty.protocols.design import gen_std_layer_design, gen_task_factory, packrotamers, struct_profile, clear_constraints, score_wnm, score_ss_sc
 
     # testing to properly set the TMPDIR on distributed jobs
     # import os
@@ -270,31 +270,30 @@ def loop_dimer(
         sw = pyrosetta.rosetta.protocols.simple_moves.SwitchChainOrderMover()
         for chains_to_loop in ['12', '34']:
             sw.chain_order(chains_to_loop)
-            pose_to_loop = deepcopy(pose)
-            sw.apply(pose_to_loop)
+            looped_pose = deepcopy(pose)
+            sw.apply(looped_pose)
 
-            loop_length = int(parent_length - pose_to_loop.chain_end(2))
+            loop_length = int(parent_length - looped_pose.chain_end(2))
 
             # is this naive? Phil did something more complicated with residue selectors, looking at the valines.
             # Wondering if I'm missing some edge cases for which this approach doesn't work.
-            loop_start = int(pose_to_loop.chain_end(1)) + 1
+            loop_start = int(looped_pose.chain_end(1)) + 1
             new_loop_str = ",".join(str(resi) for resi in range(loop_start, loop_start + loop_length))
             pyrosetta.rosetta.core.pose.setPoseExtraScore(
-                pose_to_loop, "new_loop_resis", new_loop_str
+                looped_pose, "new_loop_resis", new_loop_str
             )
 
             print_timestamp("Attempting closure by loop match...", start_time, end="")
-            closure_type = loop_match(pose_to_loop, loop_length)
+            closure_type = loop_match(looped_pose, loop_length)
             # closure by loop matching was successful, move on to the next dimer or continue to scoring
             # should I use a check like 'pose_to_loop.num_chains() == 1' to determine if the pose is closed?
             if closure_type != 'not_closed':
                 print('success.')
-                # continue
             else:
                 print('failed.')
 
                 print_timestamp("Attempting closure by loop remodel...", start_time, end="")
-                closure_type = loop_remodel(pose_to_loop, loop_length, 10, 1, 1, True)
+                closure_type = loop_remodel(looped_pose, loop_length, 10, 1, 1, True)
                 if closure_type != 'not_closed':
                     print('success.')
                 else:
@@ -302,27 +301,57 @@ def loop_dimer(
                     # couldn't close this monomer; stop trying with the whole dimer
                     break
 
-            # The code will only reach here if the loop is closed, and then I can score individual loops by accessing the pose_to_loop object
-            print_timestamp("Scoring...", start_time, end="")
-            total_length = len(pose_to_loop.residues)
-            pyrosetta.rosetta.core.pose.setPoseExtraScore(pose_to_loop, "total_length", total_length)
-            dssp = pyrosetta.rosetta.protocols.simple_filters.dssp(pose_to_loop)
-            pyrosetta.rosetta.core.pose.setPoseExtraScore(pose_to_loop, "dssp", dssp)
-            tors = get_torsions(pose_to_loop)
-            abego_str = abego_string(tors)
-            pyrosetta.rosetta.core.pose.setPoseExtraScore(pose_to_loop, "abego_str", abego_str)
-
-            # score_wnm is fine since it's only one chain
-            # should also be fast since the database is already loaded from CCM
-            score_wnm(pose_to_loop)
-            score_ss_sc(pose_to_loop, False, True, 'loop_sc')
-            print('complete.')
-
-            looped_poses.append(pose_to_loop)
+            looped_poses.append(looped_pose)
 
         # if we couldn't close the dimer, continue to the next pose and skip scoring, labeling, and yielding the pose (so nothing is written to disk)
         if closure_type == 'not_closed':
             continue
+
+        # The code will only reach here if both loops are closed.
+        # Loop closure is fast but has a high failure rate, so more efficient to first see if all loops can be closed, 
+        # and only design and score if so.
+         
+        layer_design = gen_std_layer_design()
+        design_sfxn = pyrosetta.create_score_function("beta_nov16.wts")
+        design_sfxn.set_weight(
+            pyrosetta.rosetta.core.scoring.ScoreType.res_type_constraint, 1.0
+        )
+
+        for looped_pose in looped_poses:
+
+            print_timestamp("Designing loop...", start_time, end="")
+            new_loop_sel = pyrosetta.rosetta.core.select.residue_selector.ResidueIndexSelector(new_loop_str)
+            design_sel = pyrosetta.rosetta.core.select.residue_selector.NeighborhoodResidueSelector(new_loop_sel, 6, True)
+            task_factory = gen_task_factory(
+                design_sel=design_sel,
+                pack_nbhd=True,
+                extra_rotamers_level=2,
+                limit_arochi=True,
+                prune_buns=True,
+                upweight_ppi=False,
+                restrict_pro_gly=False,
+                ifcl=False,
+                layer_design=layer_design,
+            )
+            struct_profile(looped_pose, design_sel) # Phil's code used eliminate_background=False...
+            packrotamers(looped_pose, task_factory, design_sfxn)
+            clear_constraints(looped_pose)
+            print('complete.')
+            
+            print_timestamp("Scoring...", start_time, end="")
+            total_length = len(looped_pose.residues)
+            pyrosetta.rosetta.core.pose.setPoseExtraScore(looped_pose, "total_length", total_length)
+            dssp = pyrosetta.rosetta.protocols.simple_filters.dssp(looped_pose)
+            pyrosetta.rosetta.core.pose.setPoseExtraScore(looped_pose, "dssp", dssp)
+            tors = get_torsions(looped_pose)
+            abego_str = abego_string(tors)
+            pyrosetta.rosetta.core.pose.setPoseExtraScore(looped_pose, "abego_str", abego_str)
+
+            # score_wnm is fine since it's only one chain
+            # should also be fast since the database is already loaded from CCM
+            score_wnm(looped_pose)
+            score_ss_sc(looped_pose, False, True, 'loop_sc')
+            print('complete.')
 
         combined_looped_pose = deepcopy(looped_poses[0])
         pyrosetta.rosetta.core.pose.append_pose_to_pose(combined_looped_pose, looped_poses[1], True)
@@ -338,3 +367,122 @@ def loop_dimer(
 
         ppose = io.to_packed(combined_looped_pose)
         yield ppose
+
+
+# Old, pre-design-after-looping
+# @requires_init
+# def loop_dimer(
+#     packed_pose_in: Optional[PackedPose] = None, **kwargs
+# ) -> Generator[PackedPose, PackedPose, None]:
+
+#     from copy import deepcopy
+#     from time import time
+#     import sys
+#     import pyrosetta
+#     import pyrosetta.distributed.io as io
+
+#     sys.path.insert(0, "/mnt/home/broerman/projects/crispy_shifty")
+#     from crispy_shifty.protocols.cleaning import path_to_pose_or_ppose
+#     from crispy_shifty.utils.io import print_timestamp
+#     from crispy_shifty.protocols.design import score_wnm, score_ss_sc
+
+#     # testing to properly set the TMPDIR on distributed jobs
+#     # import os
+#     # os.environ['TMPDIR'] = '/scratch'
+#     # print(os.environ['TMPDIR'])
+
+#     start_time = time()
+
+#     # generate poses or convert input packed pose into pose
+#     if packed_pose_in is not None:
+#         poses = [io.to_pose(packed_pose_in)]
+#         pdb_path = "none"
+#     else:
+#         pdb_path = kwargs["pdb_path"]
+#         poses = path_to_pose_or_ppose(
+#             path=pdb_path, cluster_scores=True, pack_result=False
+#         )
+
+#     for pose in poses:
+#         scores = dict(pose.scores)
+#         pyrosetta.rosetta.core.pose.clearPoseExtraScores(pose)
+#         # get parent length from the score
+#         parent_length = int(float(scores["parent_length"]))
+
+#         looped_poses = []
+#         sw = pyrosetta.rosetta.protocols.simple_moves.SwitchChainOrderMover()
+#         for chains_to_loop in ['12', '34']:
+#             sw.chain_order(chains_to_loop)
+#             looped_pose = deepcopy(pose)
+#             sw.apply(looped_pose)
+
+#             loop_length = int(parent_length - looped_pose.chain_end(2))
+
+#             # is this naive? Phil did something more complicated with residue selectors, looking at the valines.
+#             # Wondering if I'm missing some edge cases for which this approach doesn't work.
+#             loop_start = int(looped_pose.chain_end(1)) + 1
+#             new_loop_str = ",".join(str(resi) for resi in range(loop_start, loop_start + loop_length))
+#             pyrosetta.rosetta.core.pose.setPoseExtraScore(
+#                 looped_pose, "new_loop_resis", new_loop_str
+#             )
+
+#             print_timestamp("Attempting closure by loop match...", start_time, end="")
+#             closure_type = loop_match(looped_pose, loop_length)
+#             # closure by loop matching was successful, move on to the next dimer or continue to scoring
+#             # should I use a check like 'pose_to_loop.num_chains() == 1' to determine if the pose is closed?
+#             if closure_type != 'not_closed':
+#                 print('success.')
+#                 # continue
+#             else:
+#                 print('failed.')
+
+#                 print_timestamp("Attempting closure by loop remodel...", start_time, end="")
+#                 closure_type = loop_remodel(looped_pose, loop_length, 10, 1, 1, True)
+#                 if closure_type != 'not_closed':
+#                     print('success.')
+#                 else:
+#                     print('failed. Exiting.')
+#                     # couldn't close this monomer; stop trying with the whole dimer
+#                     break
+
+#             # The code will only reach here if the loop is closed, and then I can score individual loops by accessing the looped_pose object
+#             print_timestamp("Designing loop...", start_time, end="")
+#             new_loop_sel = pyrosetta.rosetta.core.select.residue_selector.ResidueIndexSelector(new_loop_str)
+#             designable_sel = pyrosetta.rosetta.core.select.residue_selector.NeighborhoodResidueSelector(new_loop_sel, 6, True)
+            
+            
+#             print_timestamp("Scoring...", start_time, end="")
+#             total_length = len(looped_pose.residues)
+#             pyrosetta.rosetta.core.pose.setPoseExtraScore(looped_pose, "total_length", total_length)
+#             dssp = pyrosetta.rosetta.protocols.simple_filters.dssp(looped_pose)
+#             pyrosetta.rosetta.core.pose.setPoseExtraScore(looped_pose, "dssp", dssp)
+#             tors = get_torsions(looped_pose)
+#             abego_str = abego_string(tors)
+#             pyrosetta.rosetta.core.pose.setPoseExtraScore(looped_pose, "abego_str", abego_str)
+
+#             # score_wnm is fine since it's only one chain
+#             # should also be fast since the database is already loaded from CCM
+#             score_wnm(looped_pose)
+#             score_ss_sc(looped_pose, False, True, 'loop_sc')
+#             print('complete.')
+
+#             looped_poses.append(looped_pose)
+
+#         # if we couldn't close the dimer, continue to the next pose and skip scoring, labeling, and yielding the pose (so nothing is written to disk)
+#         if closure_type == 'not_closed':
+#             continue
+
+#         combined_looped_pose = deepcopy(looped_poses[0])
+#         pyrosetta.rosetta.core.pose.append_pose_to_pose(combined_looped_pose, looped_poses[1], True)
+#         sw.chain_order('12')
+#         sw.apply(combined_looped_pose)
+#         pyrosetta.rosetta.core.pose.clearPoseExtraScores(combined_looped_pose)
+
+#         for key, value in scores.items():
+#             pyrosetta.rosetta.core.pose.setPoseExtraScore(combined_looped_pose, key, value)
+#         for protomer, looped_pose in zip(['A', 'B'], looped_poses):
+#             for key, value in looped_pose.scores.items():
+#                 pyrosetta.rosetta.core.pose.setPoseExtraScore(combined_looped_pose, key + '_' + protomer, value)
+
+#         ppose = io.to_packed(combined_looped_pose)
+#         yield ppose
