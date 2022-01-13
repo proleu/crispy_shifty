4f2810ed533c9a9504ef4a885cd72cda58ce5b3f
Philip Leung <pleung@uw.edu>
2022-01-13 12:33:18 -0800

    add build yml that works and test for env

---
+++ envs/test_crispy.py
@@ -0,0 +1,12 @@
+import pyrosetta
+pyrosetta.init()
+import dask_jobqueue
+import torch
+print(torch.__version__)
+import tensorflow as tf
+print(tf.__version__)
+from jax.lib import xla_bridge
+print(xla_bridge.get_backend().platform)
+import numpy as np
+print(np.__version__)
+import bokeh
