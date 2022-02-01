47c38b0e6a32460d65ca91ac563e0e589bce00a2
Philip Leung <pleung@uw.edu>
2022-01-31 22:15:49 -0800

    start post 05 analysis

--- crispy_shifty/protocols/msd.py
+++ crispy_shifty/protocols/msd.py
@@ -270,3 +270,71 @@ def two_state_design_paired_state(
             yield ppose
 
 
+# @requires_init
+# def mpnn_paired_state(
+#     packed_pose_in: Optional[PackedPose] = None, **kwargs
+# ) -> Iterator[PackedPose]:
+#     """
+#     :param: packed_pose_in: a PackedPose object to be interface designed with MPNN.
+#     :param: kwargs: keyword arguments to be passed to MPNNDesign, or this function.
+#     :return: an iterator of PackedPose objects.
+#     """
+# 
+#     from pathlib import Path
+#     import sys
+#     from time import time
+#     import pyrosetta
+#     import pyrosetta.distributed.io as io
+#     from pyrosetta.rosetta.core.select.residue_selector import ChainSelector
+# 
+#     # insert the root of the repo into the sys.path
+#     sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
+#     from crispy_shifty.protocols.cleaning import path_to_pose_or_ppose
+#     from crispy_shifty.protocols.design import interface_between_selectors
+#     from crispy_shifty.protocols.mpnn import MPNNDesign
+#     from crispy_shifty.utils.io import print_timestamp
+# 
+#     start_time = time()
+# 
+#     # generate poses or convert input packed pose into pose
+#     if packed_pose_in is not None:
+#         poses = [io.to_pose(packed_pose_in)]
+#         pdb_path = "none"
+#     else:
+#         pdb_path = kwargs["pdb_path"]
+#         poses = path_to_pose_or_ppose(
+#             path=pdb_path, cluster_scores=True, pack_result=False
+#         )
+# 
+#     for pose in poses:
+#         pose.update_residue_neighbors()
+#         scores = dict(pose.scores)
+#         print_timestamp("Setting up design selector", start_time)
+#         # make a designable residue selector of only the interface residues
+#         chain_a = ChainSelector(1)
+#         chain_b = ChainSelector(2)
+#         interface_selector = interface_between_selectors(chain_a, chain_b)
+#         print_timestamp("Designing interface with MPNN", start_time)
+#         # construct the MPNNDesign object
+#         mpnn_design = MPNNDesign(
+#             selector=interface_selector,
+#             omit_AAs="CX",
+#             **kwargs,
+#         )
+#         # design the pose
+#         mpnn_design.apply(pose)
+#         print_timestamp("MPNN design complete, updating pose datacache", start_time)
+#         # update the scores dict
+#         scores.update(pose.scores)
+#         # update the pose with the updated scores dict
+#         for key, value in scores.items():
+#             pyrosetta.rosetta.core.pose.setPoseExtraScore(pose, key, value)
+#         if kwargs["generate_all_poses"]:
+#             # generate all poses, each with a sequence threaded
+#             for threaded_pose in mpnn_design.generate_all_poses(pose):
+#                 ppose = io.to_packed(threaded_pose)
+#                 yield ppose
+#         else:
+#             # generate the original pose, with the sequences written to the datacache
+#             ppose = io.to_packed(pose)
+#             yield ppose
